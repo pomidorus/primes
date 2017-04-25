991eca4ae42f0b42e2f2ac86ab1f8f907c8acd8e
Andrey Seleznov <andrewaka39@gmail.com>
2017-04-25 10:41:18 +0100

    add tests for commands parser

--- test/services/test_commands_parser.rb
+++ test/services/test_commands_parser.rb
@@ -10,4 +10,15 @@ describe CommandsParser do
       assert_equal('help', @command_parser.parse)
     end
   end
+
+  describe '#parse generate' do
+    before do
+      @command_parser = CommandsParser.new('generate 10')
+    end
+
+    it 'returns valid command and args' do
+      assert_equal('generate', @command_parser.parse)
+      assert_equal(['10'], @command_parser.args)
+    end
+  end
 end
